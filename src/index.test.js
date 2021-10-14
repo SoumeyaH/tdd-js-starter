b507c7f6629dbadf3aa38fb36110d78b2620d92b
Soumeya <soumeyahassan47@gmail.com>
2021-10-14 19:02:27 +0100

    added failing test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,11 @@
-import {projectName} from "./index";
+import { projectName } from "./index";
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
+describe("Starter project should", () => {
+  it("output its name", () => {
+    expect(projectName()).toEqual("Kata starter project");
+  });
+
+  it("output its type", () => {
+    expect(projectType()).toEqual("Scrum");
   });
 });
